bcc9483e99c9fa6046357379132eb6a6a1a05b23
Laurence Unabia <laurenceunabia@gmail.com>
2024-04-23 01:02:31 +0800

    created components

---
+++ Protoapp/src/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <div class="card border-0" style="width: 20rem">
+      <div class="card-body">
+        <h1 class="card-title">Protoapp</h1>
+        <h3 class="card-text">
+          Our landing page template works on all devices, so you only have to
+          set ip up once, and get beautiful results forever.
+        </h3>
+        <a href="#" class="btn btn-primary">Get Started</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
