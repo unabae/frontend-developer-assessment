985c6b7f3741f66a90d67707660beedf718cf81d
Laurence Unabia <laurenceunabia@gmail.com>
2024-04-23 21:31:04 +0800

    added component

--- Protoapp/src/App.vue
+++ Protoapp/src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import AppStoreVue from "./AppStore.vue";
+import CardVueRight from './CardRight.vue';
 import ContentVue from "./Content.vue";
 import HeaderVue from "./Header.vue";
 import NavBarVue from "./NavBar.vue";
@@ -10,6 +11,7 @@ export default {
     NavBarVue,
     AppStoreVue,
     ContentVue,
+    CardVueRight
   },
 };
 </script>
@@ -22,6 +24,10 @@ export default {
     <ContentVue
       title="Meet Protoapp"
       body="The app lorem ipsum is common placeholder text used to demonstrate the graphic elements of a document or visual presentation."
+    />
+    <CardVueRight
+      cardTitle="Discover"
+      cardText="Get information on using these guidelines whenever you are using therefore they have the same style and quality."
     />
   </div>
 </template>
---
+++ Protoapp/src/CardRight.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container mt-5 py-5">
+    <div class="row justify-content-center">
+      <div class="col-sm-8">
+        <div class="text-center">
+          <div class="row g-0">
+            <div class="col-md-8">
+              <div class="card-body">
+                <h3 class="card-title text-end"> {{ cardTitle }} </h3>
+                <p class="card-text text-end">
+                  {{ cardText }}
+                </p>
+                <p class="card-text text-end">
+                  <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <img src="./assets/product-search.svg" class="img-fluid rounded-start" alt="...">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+    props: ['cardTitle', 'cardText']
+};
+</script>
